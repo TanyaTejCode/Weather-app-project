2971d677c6d9625465f3b5f196fb2399bb03044d
TanyaTejCode <108121299+TanyaTejCode@users.noreply.github.com>
2022-06-27 15:25:38 +0300

    Started adding date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
+}
+
 function displayTemperature(response) {
   let temperature = document.querySelector("#temperature");
   temperature.innerHTML = Math.round(response.data.main.temp);
@@ -9,7 +16,8 @@ function displayTemperature(response) {
   humidity.innerHTML = ` ${response.data.main.humidity}`;
   let wind = document.querySelector("#wind");
   wind.innerHTML = ` ${response.data.wind.speed} `;
-  console.log(response.data);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "ec60a7f865c1bc2ca6ffb4f631d54c2d";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=riga&appid=${apiKey}&units=metric`;
