70615a3d76d8cd9ce70aee0e3e2e708ced12d5d6
TanyaTejCode <108121299+TanyaTejCode@users.noreply.github.com>
2022-06-28 16:33:23 +0300

    Added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -30,8 +30,8 @@ function formatDate(timestamp) {
 function displayTemperature(response) {
   let temperature = document.querySelector("#temperature");
   temperature.innerHTML = Math.round(response.data.main.temp);
-  let city = document.querySelector("#city");
-  city.innerHTML = response.data.name;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
   let description = document.querySelector("#description");
   description.innerHTML = response.data.weather[0].description;
   let humidity = document.querySelector("#humidity");
@@ -40,8 +40,15 @@ function displayTemperature(response) {
   wind.innerHTML = ` ${response.data.wind.speed} `;
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  icon.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "ec60a7f865c1bc2ca6ffb4f631d54c2d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=riga&appid=${apiKey}&units=metric`;
+let city = "riga";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
